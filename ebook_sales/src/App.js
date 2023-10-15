a273d84e74c29a29ddacf5b236d604ebfeba232b
hassadem <hassadem001@gmail.com>
2023-10-15 18:45:37 +0100

    This is my landing page to the ebook I titled Learn Web Development in 6 Hours

---
+++ ebook_sales/src/App.js
@@ -0,0 +1,157 @@
+export default function App() {
+  return (
+    <div>
+      <Title />
+      <Subtitle />
+      <Body />
+      <Footer />
+    </div>
+  ) 
+}
+
+function Title() {
+  return (
+      <h1> Learn Web Development in 6 Hours</h1>
+    );
+    
+}
+
+function Subtitle() {
+  return <h2>A Strictly Complete Comprehensive guide on Web developemnt for Beginners </h2>
+}
+
+function Body() {
+  return (
+    <div>
+      <p>
+      Web development is the process of creating and maintaining websites. 
+      It's a broad field that encompasses everything from the front-end design of a website to the back-end server code that powers it.
+      </p>
+      <p>
+      If you're interested in learning web development, there are many different resources available to you. 
+      You can find online courses, tutorials, and books that can teach you the basics of HTML, CSS and other web development technologies.
+      </p>
+
+      <p>
+      However, if you're serious about learning web development, you need to find a resource that will teach you the skills you need to build real-world websites. 
+      That's where the Learn Web Development in 6 Hours course comes in.
+      </p>
+
+      <p>
+      This course will teach you everything you need to know to build responsive websites from scratch. 
+      You'll learn about HTML, CSS, Git, GitHub, domain and hosting setup, and more.
+      </p>
+        <em>By the end of this course, you'll be able to:</em>
+
+        <ul>
+          <li>
+          Build responsive websites from scratch
+          </li>
+          <li>
+          Use HTML, CSS, and Responsive Design approach to create dynamic web pages
+          </li>
+          <li>
+          Use Git and GitHub to manage your code
+          </li>
+          <li>
+          Set up a domain and hosting account for your website
+          </li>
+          <li>
+          Design a user-friendly and accessible website
+          </li>
+        </ul>
+        <h3>
+          What you will learn?
+          <br />👇👇👇👇👇👇👇👇👇
+        </h3>
+
+        <h2>
+        Module 1: Introduction to Web Development
+        </h2>
+        <p>
+        This module will introduce you to the basics of web development, including HTML, CSS, and Git and GitHub. 
+        You'll also learn about the different types of web development and how to choose the right one for your needs.
+        </p>
+
+        <h2>
+        Module 2: HTML
+        </h2>
+        <p>
+        HTML is the language used to create the structure of a web page. 
+        In this module, you'll learn how to use HTML to create different elements of a web page, such as headings, paragraphs, images, and links.
+        </p>
+
+        <h2>
+        Module 3: CSS
+        </h2>
+        <p>
+        CSS is the language used to style the elements of a web page. 
+        In this module, you'll learn how to use CSS to change the color, font, size, and layout of the elements on your web page.
+        </p>
+
+        <h2>
+        Module 4: Git and GitHub
+        </h2>
+        <p>
+        Git and GitHub are tools used to manage code. 
+        In this module, you'll learn how to use Git and GitHub to track your changes to code, collaborate with other developers, and deploy your code to production.
+        </p>
+
+        <h2>
+        Module 5: Domain and Hosting Setup
+        </h2>
+        <p>
+        To make your website accessible to the public, you need to register a domain name and set up a hosting account. 
+        In this module, you'll learn how to register a domain name and set up a hosting account for your website.
+        </p>
+
+        <h2>
+        Module 6: Responsive Web Design
+        </h2>
+        <p>
+        Responsive web design is the process of designing websites that look good and function well on all devices, from desktop computers to smartphones. 
+        In this module, you'll learn how to design responsive websites using CSS.
+        </p>
+
+        <em>
+        This course includes three hands-on projects that will give you the opportunity to practice the skills you've learned:
+        </em>
+
+        <h2>
+        Project 1: Build a Personal Website
+        </h2>
+        <p>
+        In this project, you'll build a personal website using HTML and CSS. 
+        You'll learn how to create different pages for your website, add content to your pages, and style your website using CSS.
+        </p>
+
+        <h2>
+        Project 2: Build a Recipe or Blog Website
+        </h2>
+        <p>
+        In this project, you'll build a blog website. You'll learn how to create a blog post, add images and other media to your blog posts, and manage your blog posts. 
+        You'll also learn how to set up a comments section so that visitors can leave comments on your blog posts.
+        </p>
+
+        <h2>
+        Project 2: Build a Landing Page
+        </h2>
+        <p>
+        In this project, you'll build a landing page website using HTML and CSS. 
+        You'll learn how to create landing page for your sales, add content to your pages, and style your website using CSS.
+        </p>
+
+        <em>
+        If you're serious about learning web development, the Learn Web Development in 6 Hours course is the right choice for you. 
+        This course will teach you everything you need to know to build responsive websites from scratch.
+        By the end of this course, you'll be able to build real-world websites and launch your own successful web development career.
+        </em>
+    </div>
+  )
+}
+
+function Footer() {
+  return <footer>
+    Click <a href="https://selar.co/ss5187" style={{backgroundColor: "#d32f", color: "green"}}>here</a> to learn more and download your ebook today!
+  </footer>
+}
